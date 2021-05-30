ebafa8eba796ff5a6c5efb675b1a5cac45abc141
Alex Kotov <kotovalexarian@gmail.com>
2021-05-31 00:03:16 +0500

    Start implementing token vector

--- token.c
+++ token.c
@@ -3,8 +3,44 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct TokenVector {
+    size_t capacity;
+    size_t count;
+    struct Token *ptr;
+};
 
 static const char *token_type_str(enum TokenType token_type);
+
+struct TokenVector *token_vector_new()
+{
+    struct TokenVector *const token_vector = malloc(sizeof(struct TokenVector));
+    if (token_vector == NULL) goto fail0;
+    memset(token_vector, 0, sizeof(struct TokenVector));
+
+    token_vector->capacity = 1000;
+    token_vector->count = 0;
+
+    token_vector->ptr = malloc(1000 * sizeof(struct Token));
+    if (token_vector->ptr == NULL) goto fail1;
+    memset(token_vector->ptr, 0, 1000 * sizeof(struct Token));
+
+fail1:
+    free(token_vector);
+fail0:
+    return NULL;
+}
+
+void token_vector_destroy(struct TokenVector *const token_vector)
+{
+    assert(token_vector != NULL);
+    assert(token_vector->ptr != NULL);
+
+    free(token_vector->ptr);
+    free(token_vector);
+}
 
 void token_println(const struct Token *const token)
 {
--- token.h
+++ token.h
@@ -13,6 +13,9 @@ struct Token {
     unsigned char value[TOKEN_VALUE_LENGTH_MAX];
 };
 
+struct TokenVector *token_vector_new();
+void token_vector_destroy(struct TokenVector *token_vector);
+
 void token_println(const struct Token *token);
 
 #endif // TOKEN_INCLUDED
