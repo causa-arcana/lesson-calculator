7e9d679fa2bba1c683048e164d01300c74c54c2a
Alex Kotov <kotovalexarian@gmail.com>
2021-06-07 12:37:48 +0500

    Remove old code

--- src/interpreter.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERPRETER_INCLUDED
-#define INTERPRETER_INCLUDED
-
-#include "ast.h"
-#include "object.h"
-
-Object interpreter_run(const struct Ast *ast, const struct AstNode *node);
-
-#endif // INTERPRETER_INCLUDED
--- src/object.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBJECT_INCLUDED
-#define OBJECT_INCLUDED
-
-typedef double Object;
-
-#define object_add(self, other) (((Object)self) + ((Object)other))
-#define object_mul(self, other) (((Object)self) * ((Object)other))
-
-#endif // OBJECT_INCLUDED
